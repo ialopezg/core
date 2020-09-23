bdc28e151b94430ff5b7648cf6b141c9010ad7f5
Isidro A. López G <me@ialopezg.com>
2020-09-23 04:43:32 -0500

    Updated examples

--- examples/index.php
+++ examples/index.php
@@ -2,11 +2,15 @@
 
 require_once '../vendor/autoload.php';
 
-$array = \ialopezg\Core\IteratorObject::toArray([
-    'log_path' => 'logs',
-    'default_view' => 'Home'
+$array = ialopezg\Core\IteratorObject::toArray([
+    'movies' => [
+        'the_thin_red_line' => [
+            'title' => 'The Thin Red Line',
+            'directed_by' => 'Terrence Malick',
+            'produced_by' => 'Robert Michael, Geisler Grant Hill, John Roberdeau',
+            'decription' => 'Adaptation of James Jones autobiographical 1962 novel, focusing on the conflict at Guadalcanal during the second World War.'
+        ]
+    ]
 ]);
 
 var_dump($array);
-
-// Produces:
